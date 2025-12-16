302a8e8241199563b2c1d57b3723666fe83aa995
Juciel Silva <juciel.silva@kaspper.com.br>
2025-12-16 15:36:21 -0300

    Atualizações auth

--- frontend/jnstore-angular/src/main.ts
+++ frontend/jnstore-angular/src/main.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from '../app/config/app.config';
-import { App } from '../app/feature/app';
+import { appConfig } from './app/config/app.config';
+import { App } from './app/feature/app';
 import { registerLocaleData } from '@angular/common';
 import localePt from '@angular/common/locales/pt';
 
